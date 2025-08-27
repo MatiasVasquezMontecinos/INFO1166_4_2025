1609e68a297156e7e87023f7ec38a554429f6ae3
MatiasVasquezMontecinos <matiasvaquez18@gmail.com>
2025-08-27 17:42:35 -0400

    Se subio la base de la simulacion, otorgada por Nicolas.

---
+++ src/main/java/com/bne/user_options_service/UserOptionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bne.user_options_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOptionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserOptionsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bne/user_options_service/UserOptionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bne.user_options_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserOptionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
