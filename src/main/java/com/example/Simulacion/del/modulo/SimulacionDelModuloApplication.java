e7f4a051a299d16cad1103eff7fe226c0ade25f0
MatiasVasquezMontecinos <matiasvaquez18@gmail.com>
2025-08-31 20:11:31 -0400

    Cambios de la base, para el comienzo de la simulacion

---
+++ src/main/java/com/example/Simulacion/del/modulo/SimulacionDelModuloApplication.java
@@ -0,0 +1,13 @@
+package com.example.Simulacion.del.modulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacionDelModuloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulacionDelModuloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Simulacion/del/modulo/SimulacionDelModuloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Simulacion.del.modulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulacionDelModuloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
