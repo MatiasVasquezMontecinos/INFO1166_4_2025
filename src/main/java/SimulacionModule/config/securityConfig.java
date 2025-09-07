95eae844c74ba45a34f0683724470f9669368bb1
MatiasVasquezMontecinos <matiasvaquez18@gmail.com>
2025-09-07 16:34:48 -0300

    Se estan modificando las ultmas face la base de datos

---
+++ src/main/java/SimulacionModule/config/securityConfig.java
@@ -0,0 +1,22 @@
+package SimulacionModule.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class securityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/usuarios/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(httpBasic -> {}); // <- esta es la forma correcta en Spring Security 6
+        return http.build();
+    }
+}
