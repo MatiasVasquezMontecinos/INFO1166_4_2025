6024cdd3e25343c0de00a8d83e91fcedbc69c0fb
narriagada2022 <narriagada2022@alu.uct.cl>
2025-09-11 14:06:47 -0300

    Archivos desechados
    Primer prototipo descartado

--- src/main/java/domain/model/Postulante.java
+++
@@ -1,9 +0,0 @@
-package domain.model;
-
-public interface Postulante {
-    String getRut();
-    String getNombre();
-    String getCorreo();
-    String getTelefono();
-    DatosCurriculares getDatosCurriculares();
-}
